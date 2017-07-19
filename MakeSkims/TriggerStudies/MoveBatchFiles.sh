efd970cc408105907db2e8b8fefa55d301146b2c
Vinay <vhegde91@gmail.com>
2017-07-19 09:58:53 -0500

    remove trigger skims

--- MakeSkims/TriggerStudies/MoveBatchFiles.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh    
-mv FileList*.txt Condor_Logs
-mv *.jdl Condor_Logs
-mv *.condor Condor_Logs
-mv *.stderr Condor_Logs
-mv *.stdout Condor_Logs
--- MakeSkims/TriggerStudies/cleaUpBatchFiles.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh    
-rm FileList*.txt
-rm *.jdl
-rm *.condor
-rm *.stderr
-rm *.stdout
