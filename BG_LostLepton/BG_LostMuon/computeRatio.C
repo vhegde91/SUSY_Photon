f6be32c81a660935b851ce18829f81f6c6d96b4b
vhegde91 <vhegde91@gmail.com>
2018-04-09 21:15:00 +0530

    ratio with toys

---
+++ BG_LostLepton/BG_LostMuon/computeRatio.C
@@ -0,0 +1,55 @@
+#include "TRandom3.h"
+#include "TH1F.h"
+#include "TH2F.h"
+#include "TH1D.h"
+#include "TGraphAsymmErrors.h"
+
+
+// void SetRatioErr(TH1D*TotalBkg, TH1D*Pred, TGraphAsymmErrors &Closure,
+                 //int nBins = 4 , double lowerBound = 100. , double upperBound = 900.,
+//                 bool verbose = false){
+void computeRatio(TString fName,
+                 //int nBins = 4 , double lowerBound = 100. , double upperBound = 900.,
+                 bool verbose = false){
+
+  TFile *f=new TFile(fName);
+  TH1D *TotalBkg=(TH1D*)f->Get("AllSBins_v7_Mu0");
+  TH1D *Pred=(TH1D*)f->Get("AllSBins_v7_Mu1");
+  TGraphAsymmErrors *Closure = new TGraphAsymmErrors();
+    TRandom3 rand;
+    const int nBins =TotalBkg->GetNbinsX(); 
+    double bins[nBins+1];
+    for( int i = 1 ; i <= nBins ; i++ ){
+        bins[i-1] = TotalBkg->GetBinLowEdge(i);
+    }
+    bins[nBins] = TotalBkg->GetBinLowEdge(nBins)+TotalBkg->GetBinWidth(nBins);
+
+    TH1D*hratio=new TH1D("hratio", "", nBins,bins);
+    TH2F* kappaDist = new TH2F("kappaDist","kappaDist",nBins,bins,1000,0.,5.);
+    kappaDist->Reset();
+    for( int m=1; m<=nBins; ++m){
+        for(int i=0; i<10000; ++i){
+            float num=rand.Gaus(TotalBkg->GetBinContent(m),TotalBkg->GetBinError(m));
+            float den=rand.Gaus(Pred->GetBinContent(m),Pred->GetBinError(m));
+            if(TotalBkg->GetBinError(m)<0.00001 && TotalBkg->GetBinContent(m)<0.0001){
+                num=1.0;
+                den=rand.Gaus(Pred->GetBinContent(m),Pred->GetBinError(m));
+                den=1.0+den;
+            }
+            if( TotalBkg->GetBinCenter(m)<bins[nBins] && TotalBkg->GetBinContent(m) > 0.0001 && Pred->GetBinContent(m) > 0.0001 )
+                kappaDist->Fill(TotalBkg->GetBinCenter(m),num/den);
+        }
+        TH1D*METBinKappa=(TH1D*)kappaDist->QuantilesX(0.5, "METBinKappa");
+        TH1D*METBinKappaUncUp=(TH1D*)kappaDist->QuantilesX(0.5+0.34, "METBinKappaUnc");
+        TH1D*METBinKappaUncDn=(TH1D*)kappaDist->QuantilesX(0.5-0.34, "METBinKappaUnc");
+        float RatioErrorUp=fabs(METBinKappaUncUp->GetBinContent(m)-METBinKappa->GetBinContent(m));
+        float RatioErrorDn=METBinKappa->GetBinContent(m)-METBinKappaUncDn->GetBinContent(m);
+        Closure->SetPoint(m-1, TotalBkg->GetBinCenter(m), METBinKappa->GetBinContent(m));
+        Closure->SetPointError(m-1, (bins[m]-bins[m-1])/2., (bins[m]-bins[m-1])/2., RatioErrorDn,RatioErrorUp);
+	//        if(m==4) Closure->SetPointError(m-1, (bins[m]-bins[m-1])/2.,(bins[m]-bins[m-1])/2., RatioErrorDn,RatioErrorUp);
+        if( verbose )
+	  std::cout<<m<<" Mean: "<<METBinKappa->GetBinContent(m)<<" +/- "<< RatioErrorUp<<" Asym:"<<RatioErrorUp-RatioErrorDn<<std::endl;
+    }
+    kappaDist->Draw();
+    //    Closure->Draw("AP");
+}
